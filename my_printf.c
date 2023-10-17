9999ca6c1898d2fb08c06dd012fc250ca9fa888d
yossef-mosa <yossefmosa8@gmail.com>
2023-10-17 09:32:54 -0700

    yossef

--- my_printf.c
+++ my_printf.c
@@ -1,4 +1,11 @@
 #include "main.h"
+#include <stdarg.h>
+#include <stdio.h>
+
+int print_i(int value);
+void print_b(char buffer[], int *buff_ind);
+
+#define BUFF_SIZE 1024
 /**
  * _printf - my printf function
  *
@@ -81,4 +88,26 @@ int _printf(const char *format, ...)
 	va_end(args);
 
 	return (count);
+}
+int print_i(int value)
+{
+    char buffer[32];
+    int printed = snprintf(buffer, sizeof(buffer), "%d", value);
+    for (int i = 0; i < printed; i++)
+    {
+        putchar(buffer[i]);
+    }
+    return printed;
+}
+void print_b(char buffer[], int *buff_ind)
+{
+    if (*buff_ind > 0)
+    {
+        for (int i = 0; i < *buff_ind; i++)
+        {
+            putchar(buffer[i]);
+        }
+    }
+
+    *buff_ind = 0;
 }
